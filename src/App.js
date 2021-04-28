ab165b753b7545ad701eaa26a8cfa7156e934b4c
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2021-04-28 10:48:22 -0500

    chore: config notification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import notifier from 'zeit-toast-clone'
 
 export default function App() {
-  return <div className="text-2xl">Hello World</div>
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <button
+        className="bg-purple-300 rounded-sm p-2 uppercase text-sm font-semibold"
+        onClick={() =>
+          notifier.success('Say hello to my little friend. by Scareface')
+        }
+      >
+        Show message
+      </button>
+    </div>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import 'zeit-toast-clone/styles.css'
+import 'tailwindcss/tailwind.css'
 import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { NotifierPortal } from 'zeit-toast-clone'
 import App from './app'
 
 ReactDOM.render(
   <React.StrictMode>
+    <NotifierPortal />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
