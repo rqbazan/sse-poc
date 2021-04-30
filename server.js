fbd15d41d8714d46107fb247ed565e69e81ae772
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2021-04-30 16:54:46 -0500

    feat: new ui

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http')
+
+http
+  .createServer((request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Request-Method', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+
+    if (request.method === 'OPTIONS') {
+      response.writeHead(200)
+      response.end()
+      return
+    }
+
+    console.log('last-event-id', request.headers['last-event-id'])
+
+    response.writeHead(200, {
+      Connection: 'keep-alive',
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+    })
+
+    let id = request.headers['last-event-id']
+      ? Number(request.headers['last-event-id']) + 1
+      : 1
+
+    // Send event every 3 seconds or so forever...
+    const interval = setInterval(() => {
+      response.write(`event: myEvent\nid: ${id}\ndata:This is event ${id}.`)
+      response.write('\n\n')
+      id++
+    }, 3000)
+
+    request.on('close', () => {
+      clearInterval(interval)
+      response.end()
+      console.log('Stopped sending events as client closed the connection.')
+    })
+  })
+  .listen(5000)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import React from 'react'
+import * as React from 'react'
 import notifier from 'zeit-toast-clone'
 import { CircularProgressbar } from 'react-circular-progressbar'
+import { EventSourcePolyfill } from 'event-source-polyfill'
+import { useForm } from 'react-hook-form'
 
-export default function App() {
+function connect({ URL, headers, eventName }, eventHandler) {
+  const conn = new EventSourcePolyfill(URL, { headers })
+
+  conn.addEventListener(eventName, eventHandler)
+
+  return conn
+}
+
+function Stream({ config, onClose }) {
+  const [isConnected, setIsConnected] = React.useState(false)
+
+  const [stream, setStream] = React.useState('')
+
+  const connRef = React.useRef()
+
   return (
-    <div className="max-w-3xl mx-auto p-6 flex flex-col">
+    <div className="text-gray-800 bg-blue-300 p-4 relative">
+      <div className="absolute right-4">
+        <button onClick={onClose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
+      </div>
+      <p>
+        <span className="font-semibold">URL:</span> {config.URL}
+      </p>
+      <p>
+        <span className="font-semibold">Event Name:</span> {config.eventName}
+      </p>
       <div>
+        <span className="font-semibold">Headers:</span>
+        <pre className="font-mono">
+          {JSON.stringify(config.headers, null, 2)}
+        </pre>
+      </div>
+      <p className="bg-white p-4 mt-3">{stream}</p>
+      <div className="flex justify-end mt-3">
         <button
           className="bg-purple-300 rounded-sm p-2 uppercase text-sm font-semibold"
           onClick={() => {
-            console.log('hola')
-            notifier.success('Say hello to my little friend. by Scareface')
+            if (isConnected) {
+              connRef.current.close()
+            } else {
+              connRef.current = connect(config, (e) => {
+                try {
+                  setStream((v) =>
+                    v === ''
+                      ? JSON.stringify(e.data)
+                      : `${v}\n${JSON.stringify(e.data)}`
+                  )
+                } catch (error) {
+                  notifier.error(error.message)
+                }
+              })
+            }
+            setIsConnected((v) => !v)
           }}
         >
-          Show message
+          {isConnected ? 'DISCONNECT' : 'CONNECT'}
         </button>
       </div>
-      <div className="mt-8 flex justify-center items-center">
-        <div className="h-36 w-36">
-          <CircularProgressbar value={50} text="50%" />
+    </div>
+  )
+}
+
+export default function App() {
+  const configCount = React.useRef(0)
+
+  const { register, handleSubmit } = useForm()
+
+  const [streamConfigs, setStreamConfigs] = React.useState([])
+
+  function onSubmit(formValues) {
+    try {
+      const newConfig = {
+        ...formValues,
+        id: configCount.current++,
+        headers: JSON.parse(formValues.headers),
+      }
+
+      setStreamConfigs((prevStreamConfigs) => [...prevStreamConfigs, newConfig])
+    } catch (err) {
+      notifier.error(err.message)
+    }
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto p-6 flex flex-col">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="border border-gray-400 p-4 space-y-2"
+      >
+        <div className="flex flex-col">
+          <label className="text-gray-200">URL</label>
+          <input
+            className="h-8 border border-blue-400 px-2"
+            required
+            {...register('URL')}
+          />
         </div>
+        <div className="flex flex-col">
+          <label className="text-gray-200">Event Name</label>
+          <input
+            className="h-8 border border-blue-400 px-2"
+            required
+            {...register('eventName')}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-200">Headers</label>
+          <textarea
+            className="border border-blue-400 p-2"
+            rows={3}
+            required
+            {...register('headers')}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button className="bg-purple-300 rounded-sm p-2 uppercase text-sm font-semibold">
+            NEW CONFIG
+          </button>
+        </div>
+      </form>
+      <div className="space-y-3 mt-4">
+        {streamConfigs.map((config) => (
+          <Stream
+            key={config.id}
+            config={config}
+            onClose={() =>
+              setStreamConfigs((prevStreamConfigs) =>
+                prevStreamConfigs.filter((item) => item.id !== config.id)
+              )
+            }
+          />
+        ))}
       </div>
     </div>
   )
